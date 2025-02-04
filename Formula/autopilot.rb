61654b571300e40c13f0dd5601904d1f156cc71c
Mindaugas <3825716+mindaugasrukas@users.noreply.github.com>
2025-02-03 22:21:05 -0800

    Bump release to v0.5.0

--- Formula/autopilot.rb
+++ Formula/autopilot.rb
@@ -1,8 +1,8 @@
 class Autopilot < Formula
   desc "AutoPilot allows you to execute runbooks with minimal setup and maximum flexibility. A system for gradual runbook automation."
   homepage "https://github.com/StencilFrame/autopilot"
-  url "https://github.com/StencilFrame/autopilot/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "0e493e92c0ea787c71657fa896104ef9dd9100f68dc13ed2a34f96ea95ed763a"
+  url "https://github.com/StencilFrame/autopilot/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "d518af81c98e4ab3046f11646ecc6be5a76788dc3c3b9b44f3fca17bd8924ced"
   license "Apache-2.0"
   head "https://github.com/StencilFrame/autopilot.git", branch: "main"
 
